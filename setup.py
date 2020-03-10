b1a4795710e72de7b80d435dbe6d27339e270e6d
alexjnally <44419319+alexjnally@users.noreply.github.com>
2020-03-10 11:30:35 -0700

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+import versioneer
+
+requirements = [
+    # package requirements go here
+]
+
+setup(
+    name='apac',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description="Alex's modules",
+    license="MIT",
+    author="Alex Nally",
+    author_email='alexjnally@gmail.com',
+    url='https://github.com/alexjnally/apac',
+    packages=['apac'],
+    entry_points={
+        'console_scripts': [
+            'apac=apac.cli:cli'
+        ]
+    },
+    install_requires=requirements,
+    keywords='apac',
+    classifiers=[
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+    ]
+)
