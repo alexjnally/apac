6c037189d5ce6974aa353f7295bed5977ae61ce3
alexjnally <44419319+alexjnally@users.noreply.github.com>
2020-03-10 13:53:28 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 import versioneer
 
 requirements = [
-    # package requirements go here
+    'pandas', 'os', 'pyodbc', 'xlsxwriter, 'time', 'xml.etree.ElementTree'
 ]
 
 setup(
